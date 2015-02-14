1c9ea3453f37da107215f3820792d9a83dbce944
Matt Fenlon <nolnefm@gmail.com>
2015-02-14 17:12:05 -0500

    Added Facebook ShareDialog w/FeedDialog failover.

--- src/io/branch/branchster/MonsterViewerActivity.java
+++ src/io/branch/branchster/MonsterViewerActivity.java
@@ -8,24 +8,35 @@ import io.fabric.sdk.android.Fabric;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.facebook.FacebookException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
+import com.facebook.widget.WebDialog;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
+import com.facebook.Session;
+
 public class MonsterViewerActivity extends FragmentActivity {
+
+    private static String TAG = MonsterViewerActivity.class.getSimpleName();
+
+    // Facebook
+    private UiLifecycleHelper uiHelper;
 
 	TextView txtName;
 	TextView txtDescription;
@@ -149,7 +160,108 @@ public class MonsterViewerActivity extends FragmentActivity {
                 });
             }
         });
+
+        // Share via Facebook.
+        uiHelper = new UiLifecycleHelper(this, null);
+        uiHelper.onCreate(savedInstanceState);
+        cmdFacebook.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Branch.getInstance(getApplicationContext()).getContentUrl("sms", prepareBranchDict(), new BranchLinkCreateListener() {
+                    @Override
+                    public void onLinkCreate(String url, BranchError error) {
+                        shareViaFacebook(url, "Check out my Branchster named " + monsterName);
+                    }
+                });
+            }
+        });
 	}
+
+    private void shareViaFacebook(String url, String description){
+
+        if (FacebookDialog.canPresentShareDialog(getApplicationContext(),
+                FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) {
+            publishShareDialog(url, description);
+        } else {
+            Log.w(TAG, "Unable to launch Facebook App to share Branchster. Falling back to FeedDialog.");
+            publishFeedDialog(url, description);
+        }
+
+    }
+
+    // Facebook ShareDialog - the first choice for sharing via Facebook.
+    private void publishShareDialog(String url, String description){
+        FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this)
+                .setLink(url)
+                .setDescription(description)
+                .build();
+        uiHelper.trackPendingDialogCall(shareDialog.present());
+    }
+
+    // Facebook FeedDialog fallback for if ShareDialog is not available.
+    private void publishFeedDialog(String url, String description) {
+        Bundle params = new Bundle();
+        params.putString("description", description);
+        params.putString("link", url);
+
+        WebDialog feedDialog = (
+                new WebDialog.FeedDialogBuilder(this,
+                        Session.getActiveSession(),
+                        params))
+                .setOnCompleteListener(new WebDialog.OnCompleteListener() {
+
+                    @Override
+                    public void onComplete(Bundle values,
+                                           FacebookException error) {
+                        if (error == null) {
+                            // When the story is posted, echo the success
+                            // and the post Id.
+                            final String postId = values.getString("post_id");
+                            if (postId != null) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Posted story, id: " + postId,
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                // User clicked the Cancel button
+                                Toast.makeText(getApplicationContext(),
+                                        "Publish cancelled",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (error instanceof FacebookOperationCanceledException) {
+                            // User clicked the "x" button
+                            Toast.makeText(getApplicationContext(),
+                                    "Publish cancelled",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Generic, ex: network error
+                            Toast.makeText(getApplicationContext(),
+                                    "Error posting story",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                })
+                .build();
+        feedDialog.show();
+    }
+
+    // Required to receive the callback from the Facebook app if supported.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+            @Override
+            public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+                Log.e("Activity", String.format("Error: %s", error.toString()));
+            }
+
+            @Override
+            public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+                Log.i("Activity", "Success!");
+            }
+        });
+    }
 	
 	@Override
 	protected void onStart() {
@@ -184,4 +296,36 @@ public class MonsterViewerActivity extends FragmentActivity {
 		
 		return monsterMetadata;
 	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Required for Facebook.
+        uiHelper.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Required for Facebook.
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Required for Facebook.
+        uiHelper.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Required for Facebook.
+        uiHelper.onDestroy();
+    }
 }
