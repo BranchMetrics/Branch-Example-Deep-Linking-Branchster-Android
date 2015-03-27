51aca981839bc9d29370ac9f730394887a75424d
Matt Fenlon <nolnefm@gmail.com>
2015-03-27 15:05:27 -0400

    Sync'ed colours with iOS, Angular versions.

--- src/io/branch/branchster/MonsterPartsFactory.java
+++ src/io/branch/branchster/MonsterPartsFactory.java
@@ -5,7 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 
-public class MonsterPartsFactory { 
+public class MonsterPartsFactory {
 	private int[] colorArray;
 	private Bitmap[] bodyArray;
 	private Bitmap[] faceArray;
@@ -14,15 +14,9 @@ public class MonsterPartsFactory {
 	private static MonsterPartsFactory factory_;
 	
 	private MonsterPartsFactory(Context context) {
-		colorArray = new int[]{ Color.argb(1, 36, 164, 221), 
-								Color.argb(1, 236, 98, 121),
-								Color.argb(1, 41, 180, 113),
-								Color.argb(1, 246, 153, 56),
-								Color.argb(1, 132, 38, 139),
-								Color.argb(1, 36, 202, 218),
-								Color.argb(1, 254, 213, 33),
-								Color.argb(1, 158, 22, 35) };
-		
+
+        colorArray = context.getResources().getIntArray(R.array.colors);
+
 		BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = false;
         //options.inSampleSize = 2;
@@ -55,7 +49,7 @@ public class MonsterPartsFactory {
 	}
 	
 	public int colorForIndex(int index) {
-	    return -colorArray[index];
+        return colorArray[index];
 	}
 
 	public String descriptionForIndex(int index) {
