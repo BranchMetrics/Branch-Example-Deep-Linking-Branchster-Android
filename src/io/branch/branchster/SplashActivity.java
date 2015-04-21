e556f6ca616b9b14248d307d03c2cf863dd5568e
sahil <sahilverma1@gmail.com>
2015-04-21 14:39:37 -0700

    remove files that shouldnt have been checked in

--- src/io/branch/branchster/SplashActivity.java
+++ src/io/branch/branchster/SplashActivity.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,7 +23,9 @@ public class SplashActivity extends Activity {
 	int messageIndex;
 	String[] loadingMessages;
 	Branch branch;
-	
+    Context mApplicationContext;
+    private static final String TAG = "SplashActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,10 +40,10 @@ public class SplashActivity extends Activity {
 
         // Get loading messages from XML definitions.
 		loadingMessages = getResources().getStringArray(R.array.loading_messages);
-		
 		txtLoading = (TextView) findViewById(R.id.txtLoading);
+        mApplicationContext = getApplicationContext();
 
-		
+
 		new Thread() {
 	        public void run() {
 	            while (true) {
@@ -66,39 +69,33 @@ public class SplashActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        branch = Branch.getInstance(this.getApplicationContext(), getResources().getString(R.string.bnc_app_key));
+        branch = Branch.getInstance(mApplicationContext, getResources().getString(R.string.bnc_app_key));
         branch.initSession(new BranchReferralInitListener() {
             @Override
             public void onInitFinished(JSONObject referringParams, BranchError error) {
-                Log.i("Branchster", "branch init complete!");
-                try {
-                    MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
-                    Intent i;
-                    if (referringParams.has("monster")) {
-
-                        prefs.setMonsterName(referringParams.getString("monster_name"));
-                        prefs.setFaceIndex(referringParams.getInt("face_index"));
-                        prefs.setBodyIndex(referringParams.getInt("body_index"));
-                        prefs.setColorIndex(referringParams.getInt("color_index"));
-
-                        i = new Intent(getApplicationContext(), MonsterViewerActivity.class);
-
+            Intent i;
+            Log.i(TAG, "Branch init complete!");
+            try {
+                MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
+                if (referringParams.has("monster")) {
+                    prefs.setMonsterName(referringParams.getString("monster_name"));
+                    prefs.setFaceIndex(referringParams.getInt("face_index"));
+                    prefs.setBodyIndex(referringParams.getInt("body_index"));
+                    prefs.setColorIndex(referringParams.getInt("color_index"));
+                    i = new Intent(mApplicationContext, MonsterViewerActivity.class);
+                } else {
+                    if (prefs.getMonsterName() == null) {
+                        prefs.setMonsterName("");
+                        i = new Intent(mApplicationContext, MonsterCreatorActivity.class);
                     } else {
-
-                        if (prefs.getMonsterName() == null) {
-                            prefs.setMonsterName("");
-
-                            i = new Intent(getApplicationContext(), MonsterCreatorActivity.class);
-                            // If no name has been saved, this user is new, so load the monster maker screen
-
-                        } else {
-                            i = new Intent(getApplicationContext(), MonsterViewerActivity.class);
-                        }
+                        i = new Intent(mApplicationContext, MonsterViewerActivity.class);
                     }
-                    startActivity(i);
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
+                startActivity(i);
+            } catch (JSONException e) {
+                Log.e(TAG, "Error with Branch.", e);
+                startActivity(new Intent(mApplicationContext, MonsterCreatorActivity.class));
+            }
             }
         }, this.getIntent().getData(), this);
     }
