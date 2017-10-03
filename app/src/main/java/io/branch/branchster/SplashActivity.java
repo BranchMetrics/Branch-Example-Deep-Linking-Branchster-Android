7bbf9ea6b71f9cef88be776b79b6719ebcb02833
sojan <sojan@branch.io>
2017-10-03 13:54:00 -0700

    Updating to 1.7.0

--- app/src/main/java/io/branch/branchster/SplashActivity.java
+++ app/src/main/java/io/branch/branchster/SplashActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,28 +18,21 @@ import io.branch.referral.Branch;
 import io.branch.referral.BranchError;
 import io.branch.referral.util.LinkProperties;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.applinks.AppLinkData;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class SplashActivity extends Activity {
-
+    
     TextView txtLoading;
     int messageIndex;
     private static final String TAG = "SplashActivity";
     ImageView imgSplash1, imgSplash2;
     Context mContext;
     final int ANIM_DURATION = 1500;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         mContext = this;
-
+        
         // Get loading messages from XML definitions.
         final String[] loadingMessages = getResources().getStringArray(R.array.loading_messages);
         txtLoading = (TextView) findViewById(R.id.txtLoading);
@@ -46,9 +40,9 @@ public class SplashActivity extends Activity {
         imgSplash2 = (ImageView) findViewById(R.id.imgSplashFactory2);
         imgSplash2.setVisibility(View.INVISIBLE);
         imgSplash1.setVisibility(View.INVISIBLE);
-
+        
     }
-
+    
     @Override
     protected void onStart() {
         super.onStart();
@@ -58,6 +52,17 @@ public class SplashActivity extends Activity {
                 //If not Launched by clicking Branch link
                 if (branchUniversalObject == null) {
                     proceedToAppTransparent();
+                } else if (linkProperties != null && !TextUtils.isEmpty(linkProperties.getControlParams().get("$web_only")) && !TextUtils.isEmpty(linkProperties.getControlParams().get("$3p"))) {
+                    String webOnlyParam = linkProperties.getControlParams().get("$web_only");
+                    String is3pParam = linkProperties.getControlParams().get("$3p");
+                    if (!TextUtils.isEmpty(webOnlyParam) && !TextUtils.isEmpty(is3pParam)) {
+                        if (Boolean.parseBoolean(webOnlyParam)) {
+                            String url = linkProperties.getControlParams().get("$original_url");
+                            WebView webView = findViewById(R.id.web_only_view);
+                            webView.setVisibility(View.VISIBLE);
+                            webView.loadUrl(url);
+                        }
+                    }
                 }
                 /* In case the clicked link has $android_deeplink_path the Branch will launch the MonsterViewer automatically since AutoDeeplinking feature is enabled.
                  * Launch Monster viewer activity if a link clicked without $android_deeplink_path
@@ -73,17 +78,17 @@ public class SplashActivity extends Activity {
             }
         }, this.getIntent().getData(), this);
     }
-
+    
     @Override
     public void onResume() {
         super.onResume();
     }
-
+    
     @Override
     public void onNewIntent(Intent intent) {
         this.setIntent(intent);
     }
-
+    
     private void proceedToApp() {
         MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
         Intent intent;
@@ -98,16 +103,16 @@ public class SplashActivity extends Activity {
         startActivity(intent);
         finish();
     }
-
+    
     private void proceedToAppTransparent() {
         Animation animSlideIn = AnimationUtils.loadAnimation(mContext, R.anim.push_down_in);
         animSlideIn.setDuration(ANIM_DURATION);
         animSlideIn.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-
+                
             }
-
+            
             @Override
             public void onAnimationEnd(Animation animation) {
                 MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
@@ -122,20 +127,20 @@ public class SplashActivity extends Activity {
                 }
                 startActivity(intent);
                 finish();
-
+                
             }
-
+            
             @Override
             public void onAnimationRepeat(Animation animation) {
-
+                
             }
         });
-
+        
         imgSplash1.setVisibility(View.VISIBLE);
         imgSplash2.setVisibility(View.VISIBLE);
         imgSplash2.startAnimation(animSlideIn);
     }
-
+    
     @Override
     protected void onPause() {
         super.onPause();
