4faebf588cbd37ea8d1fe908d2c1193636196e88
bklastaitis-branch <benas.klastaitis@branch.io>
2019-10-04 17:31:57 -0700

    added leanback support and removed play services so its comaptible with Fire OS

---
+++ app/src/main/java/io/branch/branchster/TvActivity.java
@@ -0,0 +1,12 @@
+package io.branch.branchster;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class TvActivity extends SplashActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("BranchSDK", "launched TvActivity");
+    }
+}
