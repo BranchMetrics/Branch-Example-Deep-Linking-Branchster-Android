ea648a0b628490706ff2c03a42c7e2daf9ae4a4f
bklastaitis-branch <benas.klastaitis@branch.io>
2019-10-09 14:51:37 -0700

    fixed splash screen layout and and info fragment

--- app/src/main/java/io/branch/branchster/SplashActivity.java
+++ app/src/main/java/io/branch/branchster/SplashActivity.java
@@ -1,7 +1,6 @@
 package io.branch.branchster;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,7 +9,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import io.branch.branchster.util.MonsterPreferences;
 import io.branch.indexing.BranchUniversalObject;
@@ -19,40 +17,29 @@ import io.branch.referral.BranchError;
 import io.branch.referral.util.LinkProperties;
 
 public class SplashActivity extends Activity {
+
+    private ImageView imgSplash1, imgSplash2;
+    private final int ANIM_DURATION = 1500;
     
-    TextView txtLoading;
-    int messageIndex;
-    private static final String TAG = "SplashActivity";
-    ImageView imgSplash1, imgSplash2;
-    Context mContext;
-    final int ANIM_DURATION = 1500;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mContext = this;
-        
-        // Get loading messages from XML definitions.
-        final String[] loadingMessages = getResources().getStringArray(R.array.loading_messages);
-        txtLoading = (TextView) findViewById(R.id.txtLoading);
-        imgSplash1 = (ImageView) findViewById(R.id.imgSplashFactory1);
-        imgSplash2 = (ImageView) findViewById(R.id.imgSplashFactory2);
-        imgSplash2.setVisibility(View.INVISIBLE);
-        imgSplash1.setVisibility(View.INVISIBLE);
-        
+
+        imgSplash1 = findViewById(R.id.imgSplashFactory1);
+        imgSplash2 = findViewById(R.id.imgSplashFactory2);
     }
     
-    @Override
-    protected void onStart() {
+    @Override protected void onStart() {
         super.onStart();
         Branch.getInstance().initSession(new Branch.BranchUniversalReferralInitListener() {
-            @Override
-            public void onInitFinished(BranchUniversalObject branchUniversalObject, LinkProperties linkProperties, BranchError branchError) {
+            @Override public void onInitFinished(BranchUniversalObject branchUniversalObject,
+                                                 LinkProperties linkProperties, BranchError branchError) {
                 //If not Launched by clicking Branch link
                 if (branchUniversalObject == null) {
                     proceedToAppTransparent();
-                } else if (linkProperties != null && !TextUtils.isEmpty(linkProperties.getControlParams().get("$web_only")) && !TextUtils.isEmpty(linkProperties.getControlParams().get("$3p"))) {
+                } else if (linkProperties != null &&
+                        !TextUtils.isEmpty(linkProperties.getControlParams().get("$web_only"))
+                        && !TextUtils.isEmpty(linkProperties.getControlParams().get("$3p"))) {
                     String webOnlyParam = linkProperties.getControlParams().get("$web_only");
                     String is3pParam = linkProperties.getControlParams().get("$3p");
                     if (!TextUtils.isEmpty(webOnlyParam) && !TextUtils.isEmpty(is3pParam)) {
@@ -79,42 +66,17 @@ public class SplashActivity extends Activity {
         }, this.getIntent().getData(), this);
     }
     
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-    
-    @Override
-    public void onNewIntent(Intent intent) {
+    @Override public void onNewIntent(Intent intent) {
         this.setIntent(intent);
     }
     
-    private void proceedToApp() {
-        MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
-        Intent intent;
-        if (prefs.getMonsterName() == null || prefs.getMonsterName().length() == 0) {
-            prefs.setMonsterName("");
-            intent = new Intent(SplashActivity.this, MonsterCreatorActivity.class);
-        } else {
-            // Create a default monster
-            intent = new Intent(SplashActivity.this, MonsterViewerActivity.class);
-            intent.putExtra(MonsterViewerActivity.MY_MONSTER_OBJ_KEY, prefs.getLatestMonsterObj());
-        }
-        startActivity(intent);
-        finish();
-    }
-    
     private void proceedToAppTransparent() {
-        Animation animSlideIn = AnimationUtils.loadAnimation(mContext, R.anim.push_down_in);
+        Animation animSlideIn = AnimationUtils.loadAnimation(this, R.anim.push_down_in);
         animSlideIn.setDuration(ANIM_DURATION);
         animSlideIn.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                
-            }
-            
-            @Override
-            public void onAnimationEnd(Animation animation) {
+            @Override public void onAnimationStart(Animation animation) { }
+
+            @Override public void onAnimationEnd(Animation animation) {
                 MonsterPreferences prefs = MonsterPreferences.getInstance(getApplicationContext());
                 Intent intent;
                 if (prefs.getMonsterName() == null || prefs.getMonsterName().length() == 0) {
@@ -127,23 +89,13 @@ public class SplashActivity extends Activity {
                 }
                 startActivity(intent);
                 finish();
-                
             }
-            
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                
-            }
+
+            @Override public void onAnimationRepeat(Animation animation) { }
         });
-        
+
         imgSplash1.setVisibility(View.VISIBLE);
         imgSplash2.setVisibility(View.VISIBLE);
         imgSplash2.startAnimation(animSlideIn);
-    }
-    
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
     }
 }
