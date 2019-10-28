b70f789a8f5b31bb51c509ae7a0797fc6d0c6456
bklastaitis-branch <benas.klastaitis@branch.io>
2019-10-27 23:52:52 -0700

    migrated to androidx

--- app/src/main/java/io/branch/branchster/fragment/InfoFragment.java
+++ app/src/main/java/io/branch/branchster/fragment/InfoFragment.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import io.branch.branchster.MonsterViewerActivity;
 import io.branch.branchster.R;
 
 /**
