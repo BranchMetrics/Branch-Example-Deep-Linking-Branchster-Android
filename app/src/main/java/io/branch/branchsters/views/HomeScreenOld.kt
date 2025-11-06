91219939c3319e444ba63686396544e31b8f97fc
bradleypickard-branch <bradley.pickard@branch.io>
2025-11-05 17:22:44 -0700

    minor chnages

--- app/src/main/java/io/branch/branchsters/views/HomeScreenOld.kt
+++ app/src/main/java/io/branch/branchsters/views/HomeScreenOld.kt
@@ -2,7 +2,6 @@ package io.branch.branchsters.views
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -36,7 +35,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
