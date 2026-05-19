19fa5a38c69bb986986c893c6ef98ae2ddf2e284
rob-gioia-branch <robert.gioia@branch.io>
2026-05-19 11:21:13 -0400

    Limited orientation to portrait
    Limited orientation to portrait

--- app/src/main/java/io/branch/branchster/MainActivity.kt
+++ app/src/main/java/io/branch/branchster/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.branch.branchster
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -29,6 +30,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         // ✅ Don't launch Compose until Branch is initialized
         setContent {
