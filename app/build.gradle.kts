af5dfc875a6a2b31672de1570db5a93def12cf67
gdeluna-branch <gabriel.deluna@branch.io>
2026-06-12 14:24:15 -0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.branch.branchster"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
