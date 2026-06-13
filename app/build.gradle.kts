2f0ddd77d8a6cc642dfc60ff6f0d01ae0756bf2e
gdeluna-branch <gabriel.deluna@branch.io>
2026-06-12 17:06:40 -0700

    update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.2.20-2.0.4"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20"
-    id("com.google.gms.google-services") version "4.3.15"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    id("com.google.gms.google-services") version "4.4.2"
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
